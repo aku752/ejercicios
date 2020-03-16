1612de45e3be9c4bd0dcd118195b60b2ce1b3c73
aku752 <marioluis7526@gmail.com>
2020-03-16 17:15:00 -0400

    update

--- Ejercicios propuestos/1_segundo_grado.py
+++ Ejercicios propuestos/1_segundo_grado.py
@@ -2,14 +2,19 @@ from math import sqrt
 a = int(input("Ingrese a: "))
 b = int(input("Ingrese b: "))
 c = int(input("Ingrese c: "))
-x1 = 0
-x2 = 0
 d = b*2 - 4*a*c
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
 if d > 0:
     x1 = -b + sqrt(d)/2*a
     x2 = -b - sqrt(d)/2*a
+    print("x1 = ", x1)
+    print("x2= ", x1)
 elif d == 0:
     x1 = -b/2*a
     x2 = -b/2*a
+    print("x1 = ", x1)
+    print("x2 = ", x2)
 else:
     print("Los valores son imaginarios")
