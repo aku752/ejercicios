9b0d2d4946fb4f3d5276ba55a41a62d3797cee51
aku752 <marioluis7526@gmail.com>
2020-03-28 17:09:26 -0400

    update

--- listas/5_personas.py
+++ listas/5_personas.py
@@ -4,3 +4,13 @@
 #Sea la lista:
 #		L=[“Juan ”,”Maria”,”Carlos”,”Jose”,”Carol”]
 #		L=[1, ”Juan”, 2 ,”Maria, 3, …….]
+personas = int(input("Cantidad de personas: "))
+lista = []
+for i in range(personas):
+    nombre = str(input("Escriba nombre {}: ".format(i+1)))
+    lista.append(nombre)
+j = 0
+for i in range(personas):
+    lista.insert(j, i+1)
+    j = j + 2
+print("La lista intercalada es: ", lista)
