4fa695953480f31ce7022a952fae926202eefae3
aku752 <marioluis7526@gmail.com>
2020-04-15 15:28:07 -0400

    update

--- practica comun/1_promedio.py
+++ practica comun/1_promedio.py
@@ -1,12 +1,11 @@
 lista = []
-menor = 0
 sum = 0
 for i in range(4):
     practica = int(input("Introduzca nota: "))
     lista.append(practica)
 lista.sort()
+print("Se elimina la nota de: {}".format(lista[0]))
 for i in range(1, 4):
-    print(i)
     sum = sum + lista[i]
 promedio = sum/3
 print("El promedio es: {0:.1f} ".format(promedio))
