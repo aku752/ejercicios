c5a567b8ae6181cdd27eef32c1ad09b2e779c2d7
aku752 <marioluis7526@gmail.com>
2020-04-22 10:45:22 -0400

    update

--- practica comun/26_estacionamiento.py
+++ practica comun/26_estacionamiento.py
@@ -4,12 +4,12 @@ if horas <= 2:
     total = costo*horas
 elif horas <= 5:
     costo = 4
-    total = costo*horas
+    total = (horas-2)*costo + 10
 elif horas <= 10:
     costo = 3
-    total = costo*horas
+    total = (horas-5)*costo + 22
 elif horas > 10:
     costo = 2
-    total = costo*horas
+    total = (horas-10)*costo + 31
 print("-----------------------------")
 print("Total a cobrar: ", total, "Bs")
